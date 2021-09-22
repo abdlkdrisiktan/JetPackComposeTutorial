ef20ac8a98b7317be751db5f526b1f4493bbaecd
Abdulkadir Işıktan <aisiktan@havelsan.com.tr>
2021-09-22 10:37:29 +0300

    Preview your function in Android Studio
    This annotation will mock view, This method will not change main method.

--- app/src/main/java/com/abdlkdr/jetpackcomposetutorials/MainActivity.kt
+++ app/src/main/java/com/abdlkdr/jetpackcomposetutorials/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,5 +19,11 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MessageCard(name : String) {
     Text(text = "Hello $name This is composable sample")
+}
+
+@Preview
+@Composable
+fun PreviewMessageCard() {
+    MessageCard("Android")
 }
 
