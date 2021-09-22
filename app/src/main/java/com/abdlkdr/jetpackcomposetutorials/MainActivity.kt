8ccb8f0f71f7be51994a5d63d97694a6dcc8522d
Abdulkadir Işıktan <aisiktan@havelsan.com.tr>
2021-09-22 10:33:30 +0300

    Define a composable function
    To make a function composable, add the @Composable annotation

--- app/src/main/java/com/abdlkdr/jetpackcomposetutorials/MainActivity.kt
+++ app/src/main/java/com/abdlkdr/jetpackcomposetutorials/MainActivity.kt
@@ -4,13 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Text(text = "Hello Jet Pack Compose Tutorials")
+            MessageCard(name = "Kadir")
         }
     }
+}
+
+@Composable
+fun MessageCard(name : String) {
+    Text(text = "Hello $name This is composable sample")
 }
 
